d434b123ca4d01fc35638332d132f79b88cae6ba
nimbus117 <mail@nimbus117.co.uk>
2018-10-29 15:50:02 +0000

    added film class

--- src/components/filmlist.js
+++ src/components/filmlist.js
@@ -2,13 +2,16 @@ import React, {Component} from 'react';
 import Film from './film'
 
 class FilmList extends Component {
-  filmNodes = this.props.data.map(film => {
-    return <Film key={film.id} name={film.name} url={film.url} />
-  })
   render() {
+    const filmNodes = this.props.data.map(film => {
+      return <Film key={film.id} name={film.name} url={film.url} />
+    })
     return(
       <div className="film-list">
-        {filmNodes}
+        <h2>Upcomming film releases for the UK</h2>
+        <ul>
+          {filmNodes}
+        </ul>
       </div>
     )
   }
